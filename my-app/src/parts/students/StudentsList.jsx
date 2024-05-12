65db26a1a995fc1c53d947897e3fd09b24523cbe
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-05-12 11:28:10 +0300

    7

--- my-app/src/parts/students/StudentsList.jsx
+++ my-app/src/parts/students/StudentsList.jsx
@@ -2,22 +2,21 @@ import { useSelector } from 'react-redux';
 
 export const StudentsList = () => {
     const students = useSelector((state) => state.students);
+
+    const dispStudents = students.map((student) => (
+        <div key={student.id}>
+            <h3>{student.title}</h3>
+            <p>name: {student.name}</p>
+            <p>surname: {student.surname}</p>
+            <p>age: {student.age}</p>
+            <p>occupation: {student.occupation}</p>
+        </div>
+    ));
+
+    return (
+        <div>
+            <h2>Students</h2>
+            {dispStudents}
+        </div>
+    );
 }
-
-const dispStudents = students.map((student) => (
-    <div key={student.id}>
-        <h3>{student.title}</h3>
-        <p>title: {student.title}</p>
-        <p>name: {student.name}</p>
-        <p>surname: {student.surname}</p>
-        <p>age: {student.age}</p>
-        <p>occupation: {student.occupation}</p>
-    </div>
-));
-
-return (
-    <div>
-        <h2>Students</h2>
-        {dispStudents}
-    </div>
-);
