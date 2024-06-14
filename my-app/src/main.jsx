0c02b14f15261466b5d5ca5d1a134c812028cf98
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-06-14 12:17:26 +0300

    29

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './app/store.js'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from './app/store.js';
+import { Provider } from 'react-redux';
+import { worker } from './api/server.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-)
+async function main() {
+  await worker.start({onUnhandledRequest: 'bypass'});
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  )  
+}
+
+main();
+
+
+
