9bfb792a74f31a679cf281ec4a4080574baac10b
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-06-15 12:16:48 +0300

    31

--- my-app/src/api/server.js
+++ my-app/src/api/server.js
@@ -6,12 +6,26 @@ import { nanoid } from 'nanoid';
 export const db = factory({
     student: {
         id: primaryKey(nanoid),
+        title: String,
+        name: String,
+        surname: String,
+        age: Number,
+        occupation: String,
+        votes: oneOf('vote'),
+        teacher: oneOf('teacher'),
+
     },
     teacher: {
         id: primaryKey(nanoid),
+        name: String,
+        science: String,
+        students: manyOf('student'),
     },
     vote: {
         id: primaryKey(nanoid),
+        'group leader': Number,
+        'sport captain teams': Number,
+        student: oneOf('student'),
     },
 });
 export const worker = setupWorker();
--- my-app/src/parts/teachers/teachersSlice.js
+++ my-app/src/parts/teachers/teachersSlice.js
@@ -2,8 +2,8 @@ import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = [
     {id: '0', name: 'Федоров А.В.', science: 'математика'},
-    {id: '1', name: 'Смирнов А.В.', science: 'математика'},
-    {id: '2', name: 'Матрешкин А.В.', science: 'математика'},
+    {id: '1', name: 'Смирнов А.В.', science: 'информатика'},
+    {id: '2', name: 'Матрешкин А.В.', science: 'физика'},
 ];
 
 const teachersSlice = createSlice({
