8afcaae1140ad0e6c53fb6ab5ece2d8170cb859b
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-06-15 11:51:25 +0300

    30

--- my-app/src/api/server.js
+++ my-app/src/api/server.js
@@ -1,3 +1,17 @@
 import {setupWorker} from 'msw/browser';
+import {factory, oneOf, manyOf, primaryKey} from '@mswjs/data';
+import { nanoid } from 'nanoid';
 
+
+export const db = factory({
+    student: {
+        id: primaryKey(nanoid),
+    },
+    teacher: {
+        id: primaryKey(nanoid),
+    },
+    vote: {
+        id: primaryKey(nanoid),
+    },
+});
 export const worker = setupWorker();
